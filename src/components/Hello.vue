ae5f03efb95152a6e92980fdc9194e8349b3725d
Evan Kozliner <evankozliner@gmail.com>
2017-01-31 18:03:28 -0500

    Deployment test

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <button @click="askWatson">search!</button>
+    <button @click="askWatson">search!!!!</button>
   </div>
 </template>
 
