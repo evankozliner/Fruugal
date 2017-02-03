9f1d29d54cab3e6e5fba95aa6435287ee7ced9e3
Daniel Cunningham <danielcunningham713@gmail.com>
2017-02-02 19:49:31 -0500

    Return JSON for general company info, except stock price.

--- server/api/stocks.js
+++ server/api/stocks.js
@@ -4,18 +4,21 @@ var request=require('request-promise');
 // TODO Make this return actual stock data
 // NOTE Consider seperating into seperate directory & files if this file gets too bulky
 stockRoute.get('/', (req, res) => {
-  getStock(res);
+  getGeneral(res, ticker);
 });
 
-function getStock(response) {
+var ticker = 'goog';
+
+function getGeneral(response, ticker) {
   var host = 'http://dev.markitondemand.com';
-  var pathGeneral = '/MODApis/Api/v2/Lookup/json?input=apple';
-  var pathStock = '/MODApis/Api/v2/Quote/json?symbol=aapl';
+  var pathGeneral = '/MODApis/Api/v2/Lookup/json?input=' + ticker;
+  var pathStock = '/MODApis/Api/v2/Quote/json?symbol=' + ticker;
 
   request({uri: host + pathGeneral})
     .then(function (res) {
-    response.json(JSON.parse(res));
+    response.json(JSON.parse(res)[0]);
   });
+
 }
 
 module.exports = stockRoute;
