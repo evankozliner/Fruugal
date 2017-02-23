ba79635b4a461cc1ccecaa07bbf6d68d0722b44d
Daniel Cunningham <danielcunningham713@gmail.com>
2017-02-23 18:53:44 -0500

    More work on queryExtractor - need to do entity recognition still

--- server/api/queryExtractor.js
+++ server/api/queryExtractor.js
@@ -1,6 +1,7 @@
 let masterStockHash = require('./master_stock_hash.json');
 let stopWords = require('./stopwords.json');
 let punctuation = require('./punctuation.json');
+var request=require('request-promise');
 
 // TODO This should be using entity recognition
 module.exports = class QueryExtractor {
@@ -16,22 +17,22 @@ module.exports = class QueryExtractor {
     return cleanedQuestion;
   }
 
-  extractSymbols(inputString = this.getQuestionWithoutPunctuation()) {
+  extractSymbols(inputString = this.getQuestionWithoutPunctuation(), mapObject) {
     
     return inputString
       .toUpperCase()
       .split(" ")
       .filter( (word) => {
-        return masterStockHash[word] != undefined;
+        return mapObject[word] != undefined;
       }).map((word) => {
         return {
-          marketName: masterStockHash[word],
+          marketName: mapObject[word],
           ticker: word
         };
       });
   }
 
-  getCompany(ticker) {
+  getCompany() {
 
     var spawn = require('child_process').spawn;
     var entityPython = spawn('python', ['entity_extractor.py']);
@@ -46,12 +47,38 @@ module.exports = class QueryExtractor {
 
       console.log('String returned by python file =',dataString);
 
+      //attempt to find symbol in MSH
+      var symbolArr = extractSymbols(dataString, masterStockHash);
+      if (symbolArr.length != 0) {
+        return symbolArr[0].ticker;
+      }
 
+      //attempt to find first name in MSH
+
+      var host = 'http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=' + dataString;
+
+      request({uri: host})
+      .then(function (res) {
+          var generalJSON = JSON.parse(res)[0];
+
+          if (generalJSON != undefined) {
+
+            return generalJSON.Symbol;
+
+          }
+      });
+
+      for each (pair in masterStockHash) {
+        var currentName = masterStockHash[pair];
+        var currentSymbol = pair;
+
+
+      }
 
     });
 
 
-    entityPython.stdin.write(JSON.stringify(this.question));
+    entityPython.stdin.write(JSON.stringify(this.getQuestionWithoutPunctuation()));
     entityPython.stdin.end();
 
   }
