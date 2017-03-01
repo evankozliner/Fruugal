c59dc72de982f13d6eded64aa22d12b4e61e27b1
Josh Thomas <joshthomas@Joshs-MacBook-Pro.local>
2017-03-01 01:44:07 -0500

    Passing data test

--- src/components/Stock.vue
+++ src/components/Stock.vue
@@ -3,6 +3,7 @@
     <h1>You asked a stock question!</h1>
     <h2>{{theResponse.companyName}}: {{theResponse.companySymbol}}</h2>
     <h2>Stock price: ${{theResponse.stockPrice}}</h2>
+    <h3>{{ articles }}</h3>
   </div>
 </template>
 
@@ -10,7 +11,7 @@
 export default {
 
   // The data returned from the API call
-  props: ['theResponse']
+  props: ['theResponse', 'articles']
 
 }
 </script>
--- src/components/ValidQuestion.vue
+++ src/components/ValidQuestion.vue
@@ -5,7 +5,28 @@
     </div>
 
     <div>
-      <router-view></router-view>
+      <router-view :articles="articles"></router-view>
+      <button @click="getData()">Click to pass down data to child component</button>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      articles: 'uhhhhh...'
+    }
+  },
+
+  // Method for making the call to get articles from the cluster
+  methods: {
+    getData: function () {
+      console.log('We are going to get the data')
+      this.articles = 'here they are!'
+      console.log('We have gotten the articles')
+      return this.articles
+    }
+  }
+}
+</script>
