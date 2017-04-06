b7ee92c1ac35079048929dfa6485b1d36d349337
Daniel Cunningham <danielcunningham713@gmail.com>
2017-04-05 21:23:53 -0400

    queryExtractor has NLTK and NLU in it as different versions of getCompany

--- server/api/queryExtractor.js
+++ server/api/queryExtractor.js
@@ -31,6 +31,55 @@ module.exports = class QueryExtractor {
       });
   }
 
+  //NLU ******************
+
+  /*
+  getCompany() {
+    return new Promise( (resolve, reject) => {
+      var NaturalLanguageUnderstandingV1 = require('watson-developer-cloud/natural-language-understanding/v1.js');
+      var nlu = new NaturalLanguageUnderstandingV1({
+        username: process.env.NLU_USERNAME,
+        password: process.env.NLU_PASSWORD,
+        version_date: NaturalLanguageUnderstandingV1.VERSION_DATE_2017_02_27
+      });
+      nlu.analyze({
+        text: this.getQuestionWithoutPunctuation(),
+        'features': {
+          'entities': {
+            'limit': 5
+          },
+        },
+      }, function(err, watsonResponse) {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          var i;
+          var name;
+          for (i = 0; i < watsonResponse.entities.length; i++) {
+            if (watsonResponse.entities[i].type == "Company") {
+              name = watsonResponse.entities[i].text;
+              break;
+            }
+          } 
+
+          console.log(name);
+          var host = 'http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=' + name;
+          request({uri: host}).then(function (res) {
+            var generalJSON = JSON.parse(res)[0];
+            if (generalJSON != undefined) {
+              resolve(generalJSON);
+            }
+          });
+        }
+      });
+    });
+  }
+  */
+
+
+  // NLTK *******************
+  
   getCompany() {
     // TODO extact to another method
     return new Promise( (resolve, reject) => {
@@ -71,6 +120,8 @@ module.exports = class QueryExtractor {
 
               if (generalJSON != undefined) {
 
+                console.log(generalJSON);
+
                 resolve(generalJSON);
 
               }
@@ -80,4 +131,5 @@ module.exports = class QueryExtractor {
     });
 
   }
+  
 }
